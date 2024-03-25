07a62923bf8d65e18f28b14375d78f1f4cc6592c
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2024-03-25 15:14:07 +0100

    detect range modif

--- Assets/Scripts/Enemies/DetectionRange.cs
+++ Assets/Scripts/Enemies/DetectionRange.cs
@@ -10,14 +10,11 @@ public class DetectionRange : MonoBehaviour
     
     [SerializeField] private LayerMask _playerLayer;
     
-    
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
