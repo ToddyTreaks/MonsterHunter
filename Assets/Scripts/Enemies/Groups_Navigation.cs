5724365ec9afa2e67adb72ffd318bf7e56cd5dc8
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2024-04-17 18:20:16 +0200

    create spell

--- Assets/Scripts/Enemies/Groups_Navigation.cs
+++ Assets/Scripts/Enemies/Groups_Navigation.cs
@@ -12,8 +12,10 @@ public class Groups_Navigation : MonoBehaviour
     private float spawnTime = 3.5f; 
     
     
-    private bool _waypointReached = false;
     
+    
+    private bool _waypointReached = false;
+    [SerializeField] private float timeToNextRoaming;
     private float timeBetweenRoaming;
 
 
@@ -32,16 +34,23 @@ public class Groups_Navigation : MonoBehaviour
         Transform randomWayPoint = _waypoints[UnityEngine.Random.Range(0, _waypoints.Length)];
         foreach (var enemy in _enemies)
         {
-            if (!enemy.GetComponent<DetectionRange>().isPlayerDetected )
+            if (!enemy.GetComponent<DetectionRange>().isPlayerDetected && enemy.GetComponent<Mob_Navigation>().hasSpawned)
             {
                 Vector3 ecart = new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3));
                 Vector3 randomAroundWayPoint =  randomWayPoint.position + ecart;
                 enemy.GetComponent<NavMeshAgent>().SetDestination(randomAroundWayPoint);
+                timeBetweenRoaming = timeToNextRoaming;
+            }
+            else
+            {
+                timeBetweenRoaming = 0f;
             }
 
-            timeBetweenRoaming = 10f;
+            
         }
+        
         yield return new WaitForSeconds(timeBetweenRoaming);
+        
         _isOnMove = false;
     }
     
