bc5277892ac54a74299314b5a63828e0bd9c43b8
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2024-04-18 19:38:21 +0200

    try on map and useful modif

--- Assets/Scripts/Enemies/EnemyHealthSystem.cs
+++ Assets/Scripts/Enemies/EnemyHealthSystem.cs
@@ -4,25 +4,37 @@ namespace Enemies
 {
     public class EnemyHealthSystem : HealthSystem
     {
+        #region Variables
+        
         private Animator _animator;
         private static readonly int Death = Animator.StringToHash("Death");
         private static readonly int Hit = Animator.StringToHash("Hit");
 
+        #endregion
+        
+        #region Initialization
+        
         private void Start()
         {
             _animator = GetComponent<Animator>();
         }
+        
+        #endregion
+        
+        #region Overrides
 
         public override void OnDeath()
         {
             _animator.SetTrigger(Death);
+            Destroy(gameObject);
         }
 
         public override void OnHit()
         {
             _animator.SetTrigger(Hit);
         }
-    
-    
+        
+        #endregion
+        
     }
 }
